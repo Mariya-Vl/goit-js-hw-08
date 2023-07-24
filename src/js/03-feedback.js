a81a2e659520ee7608b3d0917ff950557f4791f4
Mariia <mariya.s.vlasova@gmail.com>
2023-07-24 17:24:12 +0300

    Edited
    edited task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,15 +5,11 @@ const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 let formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
 
-// loadPage();
-
 form.addEventListener('input', throttle(getFormData, 500));
 function getFormData(evt) {
   formData[evt.target.name] = evt.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-  
 }
-console.log(formData);
 
 form.addEventListener('submit', saveData);
 function saveData(evt) {
@@ -21,35 +17,15 @@ function saveData(evt) {
   console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
   form.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  }
+  formData = {};
+}
 
-// function loadPage() {
-// //   console.log(formData);
-// // console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
-// formData = localStorage.getItem(LOCALSTORAGE_KEY);
-// //   // console.log(parsedData);
-// //   // console.log(localStorage.getItem(LOCALSTORAGE_KEY));
-// //   // console.log(parsedData.email);
-// //   // console.log(parsedData.message);
-// //   // const isLsEmpty = !localStorage.length;
-// //   // console.log(isLsEmpty);
-// //   if (!parsedData) {
-// //     console.log("форма пуста");
-// //     formData = {};
-// //   } 
-// //   if (!parsedData.email) {
-// //     console.log("no email");
-// //     form.elements.email.value = "";
-// //     form.elements.message.value = parsedData.message;
-// //   }
-// //   if (!parsedData.message) {
-// //     console.log("no message");
-// //     form.elements.email.value = parsedData.email;
-// //     form.elements.message.value = "";
-// //   } else{
-// //     console.log("тоді цей текст");
-// //     console.log(form.elements.message);
-// //     form.elements.message.textContent = parsedData.message;
-// //     form.elements.email.value = parsedData.email;
-// //   }
-// }
+function loadPage() {
+  const parsedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (parsedData) {
+    form.elements.message.value = parsedData.message || '';
+    form.elements.email.value = parsedData.email || '';
+  }
+}
+
+loadPage();
